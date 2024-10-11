d9be7930f271cccbea285b2a0c72186147f27e55
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2024-10-11 22:48:50 +0500

    project

--- 3-vazifalar.py
+++ 3-vazifalar.py
@@ -1,6 +1,25 @@
-lst = [1, 2, 33, 5, 6, 7, 7]
+# # 1-task
+# lst = [1, 2, 33, 5, 6, 7, 7]
+# n=int(input("Sonni kiriting: "))
+# for i in range(len(lst)):
+#     for j in range(i+1, len(lst)):
+#         if lst[i]+lst[j]==n:
+#             print(i, j)
+#
+# # 2-task
+# txt=input("matnni kiriting: ")
+# lst=txt.split(" ")
+# lst.sort()
+# print(' '.join(lst))
+#
+# # 3-task
 n=int(input("Sonni kiriting: "))
-for i in range(len(lst)):
-    for j in range(i+1, len(lst)):
-        if lst[i]+lst[j]==n:
-            print(i, j)
+summa=0
+for i in range(1, n+1):
+   if i==n:
+       print(f"{i}={summa+i}", end="")
+       break
+   print(f"{i} + ", end="")
+   summa += i
+
+# 4-task
