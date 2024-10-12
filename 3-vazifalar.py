e090b398f1c9e54aae017e59efd98036737f9537
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2024-10-12 23:16:17 +0500

    project

--- 3-vazifalar.py
+++ 3-vazifalar.py
@@ -75,19 +75,60 @@
 #     count += int('2'*i)
 #
 # # 9-task
-pass
-
-# 10-task
-n=int(input("sonni kiriting: "))
-a=len(str(n))
-b=str(n)
-for i in range( a):
-    d=str(int(b[i])*10**(a-i-1))
-    if i+1==a:
-        print(f"{d}", end="")
-        break
-    print(f"{d} + ", end="")
-
-# 11-task
-
-
+# for num in range(1000, 10000):
+#     minglik = num // 1000
+#     yuzlik = (num // 100) % 10
+#     onlik = (num // 10) % 10
+#     birlik = num % 10
+#     if minglik != yuzlik and minglik != onlik and minglik != birlik and yuzlik != onlik and yuzlik != birlik and onlik != birlik:
+#         print(num)
+#
+# # 10-task
+# n=int(input("sonni kiriting: "))
+# a=len(str(n))
+# b=str(n)
+# for i in range( a):
+#     d=str(int(b[i])*10**(a-i-1))
+#     if i+1==a:
+#         print(f"{d}", end="")
+#         break
+#     print(f"{d} + ", end="")
+#
+# # 11-task
+# num = int(input("Sonni kiriting: "))
+# lst = []
+# for i in range(2, num + 1):
+#     while num % i == 0:
+#         lst.append(i)
+#         num //= i
+# print(lst)
+#
+# # 12-task
+# lst=[1, 2, 3]
+# a = []
+# n = len(lst)
+# for i in range(n):
+#     for j in range(n):
+#         for k in range(n):
+#             if i != j and i != k and j != k:
+#                 results = [lst[i], lst[j], lst[k]]
+#                 a.append(results)
+# print(a)
+#
+# # 13-task
+# lst = [1, 3, 4, 9, 3, 4, 0, -1, 7, 8]
+# a = []
+# b = []
+# for i in range(len(lst)):
+#     if not b or lst[i] > b[-1]:
+#         b.append(lst[i])
+#     else:
+#         if b:
+#             a.append(b)
+#             b = []
+#         b.append(lst[i])
+# if b:
+#     a.append(b)
+# for i in a:
+#     print(i)
+# print("O'sish tartibida kelgan to'plamlar soni:", len(a))
