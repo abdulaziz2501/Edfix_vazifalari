df95296c7a6dfac3b9db26c50b825e774a3d8cc3
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2024-10-12 10:33:13 +0500

    project

--- 3-vazifalar.py
+++ 3-vazifalar.py
@@ -40,3 +40,38 @@
 # print(count)
 #
 # # 6-task
+# pass
+#
+# # 7-task
+# txt=input("matnni kiriting: ")
+# character=input("harfni kiriting: ")
+# if character.islower() and len(character)==1:
+#     for i in range(len(txt)):
+#         if character in txt:
+#             txt[i].upper()
+# print(txt)
+#hali to'liq emas 6-7 masala
+#
+# # 8-task
+# n=int(input("Sonni kiriting: "))
+# count=0
+# for i in range(1, n+1):
+#     if i == n:
+#         print(f"{int('2'*i)}={count+int('2'*i)}", end="")
+#         break
+#     print(f"{int('2'*i)} + ", end="")
+#     count += int('2'*i)
+#
+# # 9-task
+pass
+
+# 10-task
+n=int(input("sonni kiriting: "))
+a=len(str(n))
+b=str(n)
+for i in range( a):
+    d=str(int(b[i])*10**(a-i-1))
+    if i == n:
+        print(f"{d}={d}", end="")
+        break
+    print(f"{d} + ", end="")
