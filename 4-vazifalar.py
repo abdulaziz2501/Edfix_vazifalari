1c015622d98be97df2cb52ec660faaabfe4de9d3
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2024-10-15 23:58:47 +0500

    project

---
+++ 4-vazifalar.py
@@ -0,0 +1,10 @@
+def dct(lst1, lst2, lst3) -> list :
+    lugat={}
+    if len(lst1)==len(lst2) and len(lst2)==len(lst3):
+        for i in range(len(lst1)):
+            lugat[lst1[i]]={lst2[i]:lst3[i]}
+    return lugat
+lst1=['S001', 'S002', 'S003', 'S004']
+lst2=['Adina Park', 'Leyton Marsh', 'Duncan Boyle', 'Saim Richards']
+lst3=[85, 98, 89, 92]
+print(dct(lst1,lst2,lst3))
